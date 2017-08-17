d1231311be3cd60f02a1153213900cd7cb295f82
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-08-17 13:34:58 +0000

    Add multiComvolution

--- src/layers/convolution.h
+++ src/layers/convolution.h
@@ -6,45 +6,14 @@
 
 namespace marian {
 
-class Convolution {
+class ConvPoolingBase {
   public:
-    Convolution(
-        const std::string& name,
-        int kernelHeight = 3,
-        int kernelWidth = 3,
-        int kernelNum = 1,
-        int paddingHeight = 0,
-        int paddingWidth = 0,
-        int strideHeight = 1,
-        int strideWidth = 1)
-      : name_(name),
-        kernelHeight_(kernelHeight),
-        kernelWidth_(kernelWidth),
-        kernelNum_(kernelNum),
-        strideHeight_(strideHeight),
-        strideWidth_(strideWidth),
-        paddingHeight_(paddingHeight),
-        paddingWidth_(paddingWidth)
-    {
-      // std::cerr << "creating conv" << __LINE__ << std::endl;
-    }
+    ConvPoolingBase(const std::string& name)
+      : name_(name)
+    {}
 
-    Expr convert2NCHW(Expr x) {
-      std::vector<size_t> newIndeces;
-      int batchDim = x->shape()[0];
-      int sentenceDim = x->shape()[2];
-
-      for (int b = 0; b < batchDim; ++b) {
-        for (int t = 0; t < sentenceDim; ++t) {
-          newIndeces.push_back((t * batchDim) + b);
-        }
-      }
-
-      Shape shape({batchDim, 1, sentenceDim, x->shape()[1]});
-      return  reshape(rows(x, newIndeces), shape);
-    }
-
-    Expr convert2Marian(Expr x, Expr originalX) {
+  protected:
+    virtual Expr convert2Marian(Expr x, Expr originalX) {
       std::vector<size_t> newIndeces;
       int batchDim = x->shape()[0];
       int sentenceDim = x->shape()[2];
@@ -60,8 +29,51 @@ class Convolution {
       return reshape(rows(pooled, newIndeces), originalX->shape());
     }
 
+    virtual Expr convert2NCHW(Expr x) {
+      std::vector<size_t> newIndeces;
+      int batchDim = x->shape()[0];
+      int sentenceDim = x->shape()[2];
+
+      for (int b = 0; b < batchDim; ++b) {
+        for (int t = 0; t < sentenceDim; ++t) {
+          newIndeces.push_back((t * batchDim) + b);
+        }
+      }
+
+      Shape shape({batchDim, 1, sentenceDim, x->shape()[1]});
+      return  reshape(rows(x, newIndeces), shape);
+    }
+
+    virtual Expr operator()(Expr x, Expr mask) = 0;
+
+  protected:
+    std::string name_;
+
+};
+
+class Convolution : public ConvPoolingBase {
+  public:
+    Convolution(
+      const std::string& name,
+      int kernelHeight = 3,
+      int kernelWidth = 3,
+      int kernelNum = 1,
+      int paddingHeight = 0,
+      int paddingWidth = 0,
+      int strideHeight = 1,
+      int strideWidth = 1)
+      : ConvPoolingBase(name),
+        kernelHeight_(kernelHeight),
+        kernelWidth_(kernelWidth),
+        kernelNum_(kernelNum),
+        strideHeight_(strideHeight),
+        strideWidth_(strideWidth),
+        paddingHeight_(paddingHeight),
+        paddingWidth_(paddingWidth)
+    {
+    }
+
     Expr operator()(Expr x) {
-      params_.clear();
       auto graph = x->graph();
 
       int layerIn = x->shape()[1];
@@ -71,8 +83,6 @@ class Convolution {
           keywords::init=inits::glorot_uniform);
       auto bias = graph->param(name_ + "_bias",  {1, kernelNum_, 1, 1},
                                keywords::init=inits::zeros);
-      params_.push_back(kernel);
-      params_.push_back(bias);
 
       auto output = convolution(x, kernel, bias,
                                 paddingHeight_, paddingWidth_,
@@ -81,8 +91,11 @@ class Convolution {
       return output;
     }
 
+    Expr operator()(Expr x, Expr mask) {
+      return this->operator()(x, mask, 1);
+    }
+
     Expr operator()(Expr x, Expr mask, int n) {
-      params_ = {};
       auto graph = x->graph();
 
       auto masked = x * mask;
@@ -109,7 +122,6 @@ class Convolution {
     }
 
   private:
-    std::vector<Expr> params_;
     std::string name_;
 
   protected:
@@ -123,7 +135,8 @@ class Convolution {
     int paddingWidth_;
 };
 
-class Pooling {
+
+class Pooling : public ConvPoolingBase {
 public:
   Pooling(
       const std::string name,
@@ -134,7 +147,7 @@ public:
       int paddingWidth = 0,
       int strideHeight = 1,
       int strideWidth = 1)
-    : name_(name),
+    : ConvPoolingBase(name),
       type_(type),
       height_(height),
       width_(width),
@@ -157,37 +170,6 @@ public:
       return nullptr;
     }
 
-    Expr convert2NCHW(Expr x) {
-      std::vector<size_t> newIndeces;
-      int batchDim = x->shape()[0];
-      int sentenceDim = x->shape()[2];
-
-      for (int b = 0; b < batchDim; ++b) {
-        for (int t = 0; t < sentenceDim; ++t) {
-          newIndeces.push_back((t * batchDim) + b);
-        }
-      }
-
-      Shape shape({batchDim, 1, sentenceDim, x->shape()[1]});
-      return  reshape(rows(x, newIndeces), shape);
-    }
-
-    Expr convert2Marian(Expr x, Expr originalX) {
-      std::vector<size_t> newIndeces;
-      int batchDim = x->shape()[0];
-      int sentenceDim = x->shape()[2];
-
-      auto pooled = reshape(x, {batchDim * sentenceDim, x->shape()[3], 1, x->shape()[1]});
-
-      for (int t = 0; t < sentenceDim; ++t) {
-        for (int b = 0; b < batchDim; ++b) {
-          newIndeces.push_back(b * sentenceDim + t);
-        }
-      }
-
-      return reshape(rows(pooled, newIndeces), originalX->shape());
-    }
-
     Expr operator()(Expr x, Expr mask) {
       params_ = {};
 
@@ -205,7 +187,6 @@ public:
                              paddingHeight_, paddingWidth_,
                              strideHeight_, strideWidth_);
       }
-      // debug(output, "output");
 
       return convert2Marian(output, x) * mask;
     }
@@ -223,6 +204,72 @@ public:
     int paddingHeight_;
     int paddingWidth_;
 
+};
+
+
+class MultiConvolution : public ConvPoolingBase {
+  public:
+    MultiConvolution(
+      const std::string& name,
+      int kernelHeight,
+      std::vector<int> kernelWidths,
+      std::vector<int> kernelNums)
+      : ConvPoolingBase(name),
+        size_(kernelNums.size()),
+        kernelHeight_(kernelHeight),
+        kernelWidths_(kernelWidths),
+        kernelNums_(kernelNums)
+    {
+    }
+
+    Expr operator()(Expr x, Expr mask) {
+      auto graph = x->graph();
+
+      auto masked = x * mask;
+      auto xNCHW = convert2NCHW(masked);
+      auto maskNCHW = convert2NCHW(mask);
+
+      int layerIn = xNCHW->shape()[1];
+      Expr input = xNCHW;
+      std::vector<Expr> outputs;
+
+      debug(xNCHW, "x NVHW");
+      for (int i = 0; i < size_; ++i) {
+        int kernelWidth = kernelWidths_[i];
+        int kernelDim = kernelNums_[i];
+        int padWidth = kernelWidth / 2;
+
+        auto kernel = graph->param(name_ + std::to_string(i),
+            {layerIn, kernelDim, kernelWidth, x->shape()[1]},
+            keywords::init=inits::ones);
+            // keywords::init=inits::glorot_uniform);
+        auto bias = graph->param(name_ + std::to_string(i) + "_bias",  {1, kernelDim, 1, 1},
+                                 keywords::init=inits::zeros);
+
+        auto output = convolution(input, kernel, bias, padWidth, 0, 1, 1);
+        auto relued = relu(output);
+        // auto output2 = max_pooling(relued, 5, 1, 0, 0, 5, 1);
+        auto output2 = max_pooling2(relued, maskNCHW, 5, kernelWidth % 2 == 0);
+        debug(output2, "pooling output: " + std::to_string(i));
+        debug(relued, "relued output: " + std::to_string(i));
+
+        outputs.push_back(output2);
+      }
+
+      auto concated = concatenate(outputs, 1);
+      debug(concated, "concated");
+
+      return concated;
+    }
+
+  private:
+    std::string name_;
+    int size_;
+
+  protected:
+    int kernelHeight_;
+    std::vector<int> kernelWidths_;
+    std::vector<int> kernelNums_;
 };
 
 }
